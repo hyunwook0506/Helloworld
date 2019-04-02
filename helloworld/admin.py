9dd872b2266029dc4c71085efdf0365134d8df54
anthony.jhw <hyunwook0506@khu.ac.kr>
2019-04-02 21:17:54 +0900

    첫번쨰 업로드 파일입니다.

---
+++ helloworld/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+
+# Register your models here.#관리자의 기능 어떤 관리자가 될 건지 설정해주는 곳
---
+++ helloworld/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# Create your models here.# 어떤 모델을 만들건지 설정
